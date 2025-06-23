39aa588661184bd34f53859f50e471b5e2c269a8
bmcalapardo <89053612+bmcalapardo@users.noreply.github.com>
2025-06-23 08:37:15 +0000

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -6,8 +6,8 @@
 
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
-
+   # Brandon Calapardo
+   
    # Input:
    # p, principal amount
    # t, time period in years
